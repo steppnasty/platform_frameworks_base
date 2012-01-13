0ddc25f99c6cc79677f1b87cf21b27c6886c9f2e
Naomi Luis <nluis@codeaurora.org>
2012-01-13 15:12:42 -0800

    SurfaceFlinger: Remove the old GraphicBuffer reference
    Whenever we are allocating a new buffer, we delete the reference to
    the previous buffer at that buffer slot. The newly allocated Graphic
    Buffer is also added at the correct buffer slot in the
    GraphicBufferAllocator.
    
    Change-Id: Ie91e61e39b18759893c773ebda9ce7d7654d4144

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2702,7 +2702,13 @@ status_t Client::destroySurface(SurfaceID sid) {
 
 // ---------------------------------------------------------------------------
 
+#ifdef QCOM_HARDWARE
+GraphicBufferAlloc::GraphicBufferAlloc() {
+    mFreedIndex = -1;
+}
+#else
 GraphicBufferAlloc::GraphicBufferAlloc() {}
+#endif
 
 GraphicBufferAlloc::~GraphicBufferAlloc() {}
 
@@ -2722,7 +2728,12 @@ sp<GraphicBuffer> GraphicBufferAlloc::createGraphicBuffer(uint32_t w, uint32_t h
     }
 #ifdef QCOM_HARDWARE
     Mutex::Autolock _l(mLock);
-    mBuffers.add(graphicBuffer);
+    if (-1 != mFreedIndex) {
+        mBuffers.insertAt(graphicBuffer, mFreedIndex);
+        mFreedIndex = -1;
+    } else {
+        mBuffers.add(graphicBuffer);
+    }
 #endif
     return graphicBuffer;
 }
@@ -2737,6 +2748,17 @@ void GraphicBufferAlloc::freeAllGraphicBuffersExcept(int bufIdx) {
     } else {
         mBuffers.clear();
     }
+    mFreedIndex = -1;
+}
+
+void GraphicBufferAlloc::freeGraphicBufferAtIndex(int bufIdx) {
+     Mutex::Autolock _l(mLock);
+     if (0 <= bufIdx && bufIdx < mBuffers.size()) {
+        mBuffers.removeItemsAt(bufIdx);
+        mFreedIndex = bufIdx;
+     } else {
+        mFreedIndex = -1;
+     }
 }
 #endif
 // ---------------------------------------------------------------------------
