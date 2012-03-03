16d000a6bd140578ea5f763e79323d8b901d4f3f
Anurag Singh <anursing@codeaurora.org>
2012-03-03 23:59:45 +0100

    frameworks/base: Add cpu governor service
    Increase sampling rate when the display turns off and set it
    back to the original value when it turns back on. This helps
    reduce power consumption for use cases such as MP3 playback with
    the display turned off.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -130,6 +130,7 @@ class ServerThread extends Thread {
         RecognitionManagerService recognition = null;
         ThrottleService throttle = null;
         NetworkTimeUpdateService networkTimeUpdater = null;
+        CpuGovernorService cpuGovernorManager = null;
 
         // Critical services...
         try {
@@ -247,6 +248,14 @@ class ServerThread extends Thread {
                 }
             }
 
+            Slog.i(TAG, "DynamicMemoryManager Service");
+            dmm = new DynamicMemoryManagerService(context);
+
+            cpuGovernorManager = new CpuGovernorService(context);
+
+            if (cpuGovernorManager == null) {
+                Slog.e(TAG, "CpuGovernorService failed to start");
+            }
         } catch (RuntimeException e) {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service", e);
