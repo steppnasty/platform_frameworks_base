gp-b2g/frameworks_base

libstagefright: Set AAC Input buffer size for httplive streaming
-High bitrate URLs for httplive have large aac input frame sizes.
-Set httplive specific input buffer size for AAC format.
-Also,reverting an earlier similar change in sw acc decoder

CRs-Fixed: 330211
Change-Id: I6bc8d95cf71b835cf2db4aec9bd5ae2fb6b2032e

media/libstagefright/mpeg2ts/ESQueue.cpp
@@ -332,13 +332,17 @@ sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnitAAC() {
             mFormat = MakeAACCodecSpecificData(
                     profile, sampling_freq_index, channel_configuration);
 
+            mFormat->setInt32(kKeyMaxInputSize, (8192 * 3));
+
             int32_t sampleRate;
             int32_t numChannels;
+            int32_t maxInputSize;
             CHECK(mFormat->findInt32(kKeySampleRate, &sampleRate));
             CHECK(mFormat->findInt32(kKeyChannelCount, &numChannels));
+            CHECK(mFormat->findInt32(kKeyMaxInputSize, &maxInputSize));
 
-            LOGI("found AAC codec config (%d Hz, %d channels)",
-                 sampleRate, numChannels);
+            LOGI("found AAC codec config (%d Hz, %d channels), Input buffer size %d",
+                 sampleRate, numChannels,maxInputSize);
         } else {
             // profile_ObjectType, sampling_frequency_index, private_bits,
             // channel_configuration, original_copy, home

steppnasty/platform_frameworks_base

media/libstagefright/mpeg2ts/ESQueue.cpp
@@ -339,13 +339,17 @@ sp<ABuffer> ElementaryStreamQueue::dequeueAccessUnitAAC() {
             mFormat = MakeAACCodecSpecificData(
                     profile, sampling_freq_index, channel_configuration);
 
+            mFormat->setInt32(kKeyMaxInputSize, (8192 * 3));
+
             int32_t sampleRate;
             int32_t numChannels;
+            int32_t maxInputSize;
             CHECK(mFormat->findInt32(kKeySampleRate, &sampleRate));
             CHECK(mFormat->findInt32(kKeyChannelCount, &numChannels));
+            CHECK(mFormat->findInt32(kKeyMaxInputSize, &maxInputSize));
 
-            ALOGI("found AAC codec config (%d Hz, %d channels)",
-                 sampleRate, numChannels);
+            ALOGI("found AAC codec config (%d Hz, %d channels), Input buffer size %d",
+                 sampleRate, numChannels,maxInputSize);
         } else {
             // profile_ObjectType, sampling_frequency_index, private_bits,
             // channel_configuration, original_copy, home