Jackeagle/caf_frameworks_base

frameworks/base/nuplayer: Enable Smooth Streaming only for HLS
- Set enable flag based on source type in setDataSource
- Pass the meta data flag to ACodec in configureCodec call

CRs-Fixed: 347204

Change-Id: Ia3635e1f45f0dc5e0de1d686eafa60f1b0d89f7b

media/libstagefright/ACodec.cpp
@@ -2139,19 +2139,24 @@ void ACodec::UninitializedState::onSetup(
 
     mCodec->mInputEOSResult = OK;
 
-    char value[PROPERTY_VALUE_MAX];
-    if(property_get("hls.enable.smooth.streaming", value, NULL) &&
-      (!strcasecmp(value, "true") || !strcmp(value, "1")) &&
-      (!strcmp("OMX.qcom.video.decoder.avc", mCodec->mComponentName.c_str())) ) {
+    int32_t value;
+    if (msg->findInt32("smooth-streaming", &value) && (value == 1) &&
+       !strcmp("OMX.qcom.video.decoder.avc", mCodec->mComponentName.c_str())) {
 
-        LOGI("Enable Smooth streaming");
-        mCodec->mSmoothStreaming = true;
-        status_t err = mCodec->InitSmoothStreaming();
-        if (err != OK) {
-           LOGE("Error in enabling smooth streaming, ignore & disable ");
-           mCodec->mSmoothStreaming = false;
+        char value_ss[PROPERTY_VALUE_MAX];
+        if (property_get("hls.disable.smooth.streaming", value_ss, NULL) &&
+           (!strcasecmp(value_ss, "true") || !strcmp(value_ss, "1"))) {
+
+            LOGW("Dont enable Smooth streaming, disable property is set");
         } else {
-            LOGI("Smooth streaming is enabled ");
+            mCodec->mSmoothStreaming = true;
+            status_t err = mCodec->InitSmoothStreaming();
+            if (err != OK) {
+                LOGE("Error in enabling smooth streaming, ignore & disable ");
+                mCodec->mSmoothStreaming = false;
+            } else {
+                LOGI("Smooth streaming is enabled ");
+            }
         }
     }
     mCodec->configureCodec(mime.c_str(), msg);

steppnasty/platform_frameworks_base

(cherry picked from commit 0a568164ae2ea758b4ac32ab06119b578d1424a3)
Change-Id: I8504122ec30858cf06b2edce1ff5cbbad7a830c3

media/libstagefright/ACodec.cpp
@@ -2139,19 +2139,24 @@ void ACodec::UninitializedState::onSetup(
 
     mCodec->mInputEOSResult = OK;
 
-    char value[PROPERTY_VALUE_MAX];
-    if(property_get("hls.enable.smooth.streaming", value, NULL) &&
-      (!strcasecmp(value, "true") || !strcmp(value, "1")) &&
-      (!strcmp("OMX.qcom.video.decoder.avc", mCodec->mComponentName.c_str())) ) {
+    int32_t value;
+    if (msg->findInt32("smooth-streaming", &value) && (value == 1) &&
+       !strcmp("OMX.qcom.video.decoder.avc", mCodec->mComponentName.c_str())) {
 
-        ALOGI("Enable Smooth streaming");
-        mCodec->mSmoothStreaming = true;
-        status_t err = mCodec->InitSmoothStreaming();
-        if (err != OK) {
-           ALOGE("Error in enabling smooth streaming, ignore & disable ");
-           mCodec->mSmoothStreaming = false;
+        char value_ss[PROPERTY_VALUE_MAX];
+        if (property_get("hls.disable.smooth.streaming", value_ss, NULL) &&
+           (!strcasecmp(value_ss, "true") || !strcmp(value_ss, "1"))) {
+
+            ALOGW("Dont enable Smooth streaming, disable property is set");
         } else {
-            ALOGI("Smooth streaming is enabled ");
+            mCodec->mSmoothStreaming = true;
+            status_t err = mCodec->InitSmoothStreaming();
+            if (err != OK) {
+                ALOGE("Error in enabling smooth streaming, ignore & disable ");
+                mCodec->mSmoothStreaming = false;
+            } else {
+                ALOGI("Smooth streaming is enabled ");
+            }
         }
     }
     mCodec->configureCodec(mime.c_str(), msg);