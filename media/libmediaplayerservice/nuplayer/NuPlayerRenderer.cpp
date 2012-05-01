Jackeagle/caf_frameworks_base

libstagefright: Enable support for HW AAC decoder for HLS
HW AAC decoder expects AAC data frame by frame but current HLS
implementation sends chunks of AAC data together to decoder
leading to playback issues on 7x27A. With this change, AAC
data is queued frame by frame to the decoder.

Change-Id: I85ff754b86c3ed8abba139791cb87e729790506e
CRs-Fixed: 338819

media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -385,7 +385,7 @@ void NuPlayer::Renderer::onDrainVideoQueue() {
     bool tooLate = (mVideoLateByUs > 40000);
 
     if (tooLate) {
-        LOGV("video late by %lld us (%.2f secs)", lateByUs, lateByUs / 1E6);
+        LOGV("video late by %lld us (%.2f secs)", mVideoLateByUs, mVideoLateByUs / 1E6);
     } else {
         LOGV("rendering video at media time %.2f secs", mediaTimeUs / 1E6);
     }

steppnasty/platform_frameworks_base

(cherry picked from commit ec382779f712253f9958acba14aa6a7302c2dbb4)
Change-Id: Ie0195f3e7ad3eef60d7934ef9ba1673ac93caab9

media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -385,7 +385,7 @@ void NuPlayer::Renderer::onDrainVideoQueue() {
     bool tooLate = (mVideoLateByUs > 40000);
 
     if (tooLate) {
-        ALOGV("video late by %lld us (%.2f secs)", lateByUs, lateByUs / 1E6);
+        ALOGV("video late by %lld us (%.2f secs)", mVideoLateByUs, mVideoLateByUs / 1E6);
     } else {
         ALOGV("rendering video at media time %.2f secs", mediaTimeUs / 1E6);
     }