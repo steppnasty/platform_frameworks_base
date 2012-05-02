6ba726278c25b51260959fb51a6423777e4a012c
ApurupaPattapu <apurupa@codeaurora.org>
2012-05-02 13:43:01 -0700

    libstagefright: Disconnect connecting datasource in reset
    - MediaSource Read is a blocking call, and it can stall
      the complete player if it takes longer. There are chances of
      seeing a ANR in streaming scenarios.
    - This change will ensure that read call does not block, by
      disconnecting datasource
    
    Change-Id: I905384fbd5ca845ab7a6bf5c6ec30cc396fc5aba
    CRs-Fixed: 358143

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -489,6 +489,10 @@ status_t AwesomePlayer::setDataSource_l(const sp<MediaExtractor> &extractor) {
 }
 
 void AwesomePlayer::reset() {
+    //Disconnect datasource
+    if (mConnectingDataSource != NULL) {
+        mConnectingDataSource->disconnect();
+    }
     Mutex::Autolock autoLock(mLock);
     reset_l();
 }
@@ -542,6 +546,10 @@ void AwesomePlayer::reset_l() {
 
     mWVMExtractor.clear();
     mCachedSource.clear();
+    if (mConnectingDataSource != NULL) {
+        mConnectingDataSource.clear();
+    }
+
     mAudioTrack.clear();
     mVideoTrack.clear();
 
@@ -2155,7 +2163,8 @@ status_t AwesomePlayer::finishSetDataSource_l() {
             dataSource = mConnectingDataSource;
         }
 
-        mConnectingDataSource.clear();
+        //keep the connecting data source pointer, so that disconnect can be called
+        //mConnectingDataSource.clear();
 
         String8 contentType = dataSource->getMIMEType();
 
