gp-b2g/frameworks_base

libstagefright: more accurate statistics logging
- statistics frames were only obtained after rendering, duration
  between frames vary a lot based on whether the frame closest to
  cutoff time is rendered or not
- fixes this problem by splitting the total frames calculation and
  fps calculation.
- fps calculation is now done onVideoEvent before anything else

Change-Id: Ieecf7f6c9314d8f4d954abb2099775166d215388
CRs-Fixed: 334379

media/libstagefright/AwesomePlayer.cpp
@@ -243,6 +243,7 @@ AwesomePlayer::AwesomePlayer()
         mStats.mLastFrameUs = 0;
         mStats.mStatisticsFrames = 0;
         mStats.mFPSSumUs = 0;
+        mStats.mTotalTime = 0;
     }
     reset();
 }
@@ -256,6 +257,7 @@ AwesomePlayer::~AwesomePlayer() {
         Mutex::Autolock autoLock(mStatsLock);
         LOGW("=========================================================");
         LOGW("Average Frames Per Second: %.4f", mStats.mFPSSumUs/((double)mStats.mStatisticsFrames));
+        LOGW("Total Frames / Total Time: %.4f", ((double)mStats.mTotalFrames*1E6)/((double)mStats.mTotalTime));
         LOGW("========================================================");
     }
 
@@ -1700,6 +1702,24 @@ void AwesomePlayer::onVideoEvent() {
     }
     mVideoEventPending = false;
 
+    if(mStatistics) {
+        Mutex::Autolock autoLock(mStatsLock);
+        int64_t now = getTimeOfDayUs(),
+                diff = now - mStats.mLastFrameUs;
+        if (diff > 50000) {
+             float fps =((mStats.mTotalFrames - mStats.mLastFrame) * 1E6)/diff;
+             LOGW("Frames per second: %.4f", fps);
+             if(mStats.mLastFrameUs != 0){
+                 ++mStats.mStatisticsFrames;
+                 mStats.mTotalTime+=diff;
+             }
+             mStats.mFPSSumUs += fps;
+             mStats.mLastFrameUs = now;
+             mStats.mLastFrame = mStats.mTotalFrames;
+         }
+    }
+
+
     if (mSeeking != NO_SEEK) {
         if (mVideoBuffer) {
             mVideoBuffer->release();
@@ -1964,18 +1984,6 @@ void AwesomePlayer::onVideoEvent() {
             logOnTime(timeUs,nowUs,latenessUs);
             mStats.mTotalFrames++;
             mStats.mConsecutiveFramesDropped = 0;
-
-            int64_t now = getTimeOfDayUs(),
-            diff = now - mStats.mLastFrameUs;
-            if (diff > 50000) {
-                float fps =((mStats.mTotalFrames - mStats.mLastFrame) * 1E6)/diff;
-                LOGW("Frames per second: %.4f", fps);
-                if(mStats.mLastFrameUs != 0)
-                    ++mStats.mStatisticsFrames;
-                mStats.mFPSSumUs += fps;
-                mStats.mLastFrameUs = now;
-                mStats.mLastFrame = mStats.mTotalFrames;
-            }
         }
     }
 
@@ -2567,7 +2575,7 @@ void AwesomePlayer::logStatistics() {
     if (mFlags & LOOPING) {LOGW("Looping Update");}
     LOGW("Mime Type: %s",mime);
     LOGW("Number of frames dropped: %lld",mStats.mNumVideoFramesDropped);
-    LOGW("Number of frames rendered: %u",mStats.mTotalFrames);
+    LOGW("Number of frames rendered: %llu",mStats.mTotalFrames);
     LOGW("=====================================================");
 }
 

steppnasty/platform_frameworks_base

(cherry picked from commit 6fdc9466874617ed7b641183e7aa597f3faec5bf)
Change-Id: I6304bc04648f3dc7bee13c598ac1d82c116d97a4

media/libstagefright/AwesomePlayer.cpp
@@ -235,6 +235,7 @@ AwesomePlayer::AwesomePlayer()
         mStats.mLastFrameUs = 0;
         mStats.mStatisticsFrames = 0;
         mStats.mFPSSumUs = 0;
+        mStats.mTotalTime = 0;
     }
     reset();
 }
@@ -248,6 +249,7 @@ AwesomePlayer::~AwesomePlayer() {
         Mutex::Autolock autoLock(mStatsLock);
         ALOGW("=========================================================");
         ALOGW("Average Frames Per Second: %.4f", mStats.mFPSSumUs/((double)mStats.mStatisticsFrames));
+        ALOGW("Total Frames / Total Time: %.4f", ((double)mStats.mTotalFrames*1E6)/((double)mStats.mTotalTime));
         ALOGW("========================================================");
     }
 
@@ -1657,6 +1659,24 @@ void AwesomePlayer::onVideoEvent() {
     }
     mVideoEventPending = false;
 
+    if(mStatistics) {
+        Mutex::Autolock autoLock(mStatsLock);
+        int64_t now = getTimeOfDayUs(),
+                diff = now - mStats.mLastFrameUs;
+        if (diff > 50000) {
+             float fps =((mStats.mTotalFrames - mStats.mLastFrame) * 1E6)/diff;
+             LOGW("Frames per second: %.4f", fps);
+             if(mStats.mLastFrameUs != 0){
+                 ++mStats.mStatisticsFrames;
+                 mStats.mTotalTime+=diff;
+             }
+             mStats.mFPSSumUs += fps;
+             mStats.mLastFrameUs = now;
+             mStats.mLastFrame = mStats.mTotalFrames;
+         }
+    }
+
+
     if (mSeeking != NO_SEEK) {
         if (mVideoBuffer) {
             mVideoBuffer->release();
@@ -1910,18 +1930,6 @@ void AwesomePlayer::onVideoEvent() {
             logOnTime(timeUs,nowUs,latenessUs);
             mStats.mTotalFrames++;
             mStats.mConsecutiveFramesDropped = 0;
-
-            int64_t now = getTimeOfDayUs(),
-            diff = now - mStats.mLastFrameUs;
-            if (diff > 250000) {
-                float fps =((mStats.mTotalFrames - mStats.mLastFrame) * 1E6)/diff;
-                ALOGW("Frames per second: %.4f", fps);
-
-                mStats.mFPSSumUs += fps;
-                mStats.mLastFrameUs = now;
-                mStats.mLastFrame = mStats.mTotalFrames;
-                ++mStats.mStatisticsFrames;
-            }
         }
     }
 
@@ -2512,11 +2520,11 @@ void AwesomePlayer::modifyFlags(unsigned value, FlagMode mode) {
 void AwesomePlayer::logStatistics() {
     const char *mime;
     mVideoTrack->getFormat()->findCString(kKeyMIMEType, &mime);
-    ALOGW("=====================================================");
-    if (mFlags & LOOPING) {ALOGW("Looping Update");}
+    LOGW("=====================================================");
+    if (mFlags & LOOPING) {LOGW("Looping Update");}
     ALOGW("Mime Type: %s",mime);
     ALOGW("Number of frames dropped: %lld",mStats.mNumVideoFramesDropped);
-    ALOGW("Number of frames rendered: %u",mStats.mTotalFrames);
+    ALOGW("Number of frames rendered: %llu",mStats.mTotalFrames);
     ALOGW("=====================================================");
 }
 