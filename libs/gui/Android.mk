bb9d70b2384659a3bf4e682eeb925e0dc3e808a2
steppnasty <steppnasty@gmail.com>
2013-03-18 20:27:15 -0500

    Removed dependancy on libqcomui

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -35,15 +35,6 @@ LOCAL_SHARED_LIBRARIES := \
 	libEGL \
 	libGLESv2
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-LOCAL_SHARED_LIBRARIES += \
-        libQcomUI
-LOCAL_C_INCLUDES := hardware/qcom/display/libqcomui
-LOCAL_CFLAGS += -DQCOM_HARDWARE
-ifeq ($(TARGET_QCOM_HDMI_OUT),true)
-LOCAL_CFLAGS += -DQCOM_HDMI_OUT
-endif
-endif
 
 LOCAL_MODULE:= libgui
 
